39aee3fb503431990a60e61f1761c307b32c37b1
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-05-22 21:20:11 +0800

    feat: move sort to chain towards reduce method

--- clean_sort_transactions.js
+++ clean_sort_transactions.js
@@ -28,42 +28,42 @@ const findDuplicateTransactions = (transactions = []) => {
   // [[1,2,3], [5,6]]
   // if we can ignore the above, then the result would be
   // [[5,6], [1,2,3]]
-  transactions.sort((a, b) => new Date(b.time) - new Date(a.time)) // sort descending
-  // .sort((a, b) => new Date(a.time) - new Date(b.time)) // sort by ascending
+  const duplicatesMap = transactions
+    .sort((a, b) => new Date(b.time) - new Date(a.time)) // sort descending
+    // .sort((a, b) => new Date(a.time) - new Date(b.time)) // sort by ascending
+    .reduce((acc, curr) => {
+      const { sourceAccount, targetAccount, amount, category, time } = curr;
+      // key to the traversed hash table as an identifier
+      // this is to ensure check if the transaction has the same:
+      // 1. sourceAccount
+      // 2. targetAccount
+      // 3. amount
+      // 4. category
+      //
+      // after that, we need to group up the transactions
+      const key = `${sourceAccount}-${targetAccount}-${amount}-${category}`;
 
-  const duplicatesMap = transactions.reduce((acc, curr) => {
-    const { sourceAccount, targetAccount, amount, category, time } = curr;
-    // key to the traversed hash table as an identifier
-    // this is to ensure check if the transaction has the same:
-    // 1. sourceAccount
-    // 2. targetAccount
-    // 3. amount
-    // 4. category
-    //
-    // after that, we need to group up the transactions
-    const key = `${sourceAccount}-${targetAccount}-${amount}-${category}`;
+      // if traversed before, check the transaction time against traversed[key].time
+      if (traversed[key]) {
+        if (Math.abs(new Date(time) - new Date(traversed[key].time)) < 60000) {
+          const duplicated = (acc[category] || []).find(t => t.id === traversed[key].id);
 
-    // if traversed before, check the transaction time against traversed[key].time
-    if (traversed[key]) {
-      if (Math.abs(new Date(time) - new Date(traversed[key].time)) < 60000) {
-        const duplicated = (acc[category] || []).find(t => t.id === traversed[key].id);
+          // check if there is duplicate in category array
+          if (!duplicated) {
+            // initialise initial category array
+            if (!acc[category]) acc[category] = []
 
-        // check if there is duplicate in category array
-        if (!duplicated) {
-          // initialise initial category array
-          if (!acc[category]) acc[category] = []
-
-          // push the traversed transaction and current transaction
-          acc[category].push(traversed[key], curr);
-        } else {
-          acc[category].push(curr);
+            // push the traversed transaction and current transaction
+            acc[category].push(traversed[key], curr);
+          } else {
+            acc[category].push(curr);
+          }
         }
       }
-    }
 
-    traversed[key] = curr;
-    return acc;
-  }, {})
+      traversed[key] = curr;
+      return acc;
+    }, {})
 
   // sort transaction in ascending after to match output (only if initially sorted descending)
   return Object.values(duplicatesMap).map((value) => {
